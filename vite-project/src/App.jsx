33d1e2c9b168c60098f9c32941fcb305373e1a1b
BushynskaIra <bushynskaira@gmail.com>
2024-07-21 13:00:21 -0600

    update

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,30 @@
+
+import Profile from './components/Profile/Profile';
+import userData from './userData.json';
+import './App.css'
+
+
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App
+
+
+
+
+
+
+
+
