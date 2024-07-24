9aae6dbad29c112b5e69bdf6790685aa40532540
BushynskaIra <bushynskaira@gmail.com>
2024-07-24 12:50:15 -0600

    change structure

--- src/components/FriendsListItem/FriendListItem.jsx
+++ src/components/FriendsListItem/FriendListItem.jsx
@@ -1,6 +1,6 @@
 
 import PropTypes from 'prop-types';
-import styles from './FriendList.module.css';
+import styles from '../FriendList/FriendList.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
     return (
